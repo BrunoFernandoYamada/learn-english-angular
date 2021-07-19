1049314c3a8a7709e8fd5b06e7460678e9d2f520
Bruno Yamada <bfyamada@outlook.com>
2021-07-19 19:38:03 -0300

    First commit

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from "../shared/frase.model";
+
+export const FRASES : Frase[] = [
+    {fraseEng: "I like to learn", frasePrBr: 'Eu gosto de aprender'},
+    {fraseEng: "I watch TV", frasePrBr: 'Eu assisto TV'},
+    {fraseEng: "How are you?", frasePrBr: 'Como vai você?'},
+    {fraseEng: "I eat bread", frasePrBr: 'Eu como pão'}
+]
